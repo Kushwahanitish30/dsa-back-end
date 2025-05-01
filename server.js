0911cd33a5e175fa9acca36573744a0f90863614
nitish kumar <kushwaha30nitish2002@gmail.com>
2025-05-01 22:03:56 +0530

    Fix:changes done

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/topics", topicRoutes);
 
 // MongoDB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => {
-//   console.log("MongoDB connected");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("MongoDB connected");
   app.listen(5000, () => console.log("Server running on port 5000"));
-// }).catch(err => console.error("DB connection error:", err));
+}).catch(err => console.error("DB connection error:", err));
