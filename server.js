03e3fe9697be1371689a3b0fa4f9a898cbceab64
nitish kumar <kushwaha30nitish2002@gmail.com>
2025-05-01 21:56:16 +0530

    fix:changes done

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => res.send("API is running"));
 app.use("/api/auth", authRoutes);
 app.use("/api/topics", topicRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("MongoDB connected");
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log("MongoDB connected");
   app.listen(5000, () => console.log("Server running on port 5000"));
-}).catch(err => console.error("DB connection error:", err));
+// }).catch(err => console.error("DB connection error:", err));
